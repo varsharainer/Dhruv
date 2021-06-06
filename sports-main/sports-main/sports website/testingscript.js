546fd58cd5209b5cf18e85bbf5fa6552093c5ff7
rainer <56734790+varsharainer@users.noreply.github.com>
2021-06-06 13:06:53 +0530

    Add files via upload

---
+++ sports-main/sports-main/sports website/testingscript.js
@@ -0,0 +1,11 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAR1mfliggtKVHZtxxL00BsVBmbfqk40Lo",
+    authDomain: "sports-website-5e29b.firebaseapp.com",
+    databaseURL: "https://sports-website-5e29b-default-rtdb.firebaseio.com",
+    projectId: "sports-website-5e29b",
+    storageBucket: "sports-website-5e29b.appspot.com",
+    messagingSenderId: "788967978992",
+    appId: "1:788967978992:web:5c3081cd193f7164d80f35"
+  };
+  firebase.initializeApp(firebaseConfig);
+
